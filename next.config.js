516b358bc10f1cda3573a5464fc16afc26a0ee45
cristea <victor.kristea@mail.ru>
2023-12-06 16:22:19 +0200

    same as front

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ module.exports = withSentryConfig(
     silent: true,
     org: "straus-org",
     project: "test",
-    release: "test@0.1.6",
+    release: "test@0.1.7",
   },
   {
     // For all available options, see:
@@ -26,22 +26,7 @@ module.exports = withSentryConfig(
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
-
     // Hides source maps from generated client bundles
     hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
   }
 );
