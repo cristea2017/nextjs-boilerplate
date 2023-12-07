e327677dd15c8a40036ab393af80568bce55f50a
cristea <victor.kristea@mail.ru>
2023-12-07 08:08:25 +0200

    0.2.1

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ module.exports = withSentryConfig(
     silent: true,
     org: "straus-org",
     project: "test",
-    release: "test@0.1.7",
+    release: "test@0.2.1",
   },
   {
     // For all available options, see:
--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,7 +10,7 @@ Sentry.init({
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 0.1,
-  release: "test@0.1.7",
+  release: "test@0.2.1",
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
